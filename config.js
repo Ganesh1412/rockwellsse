eedf9548e7ffb3234a73e4e7f303f488cf63f5c0
Ganesh1412 <ganeshkbdecember14@gmail.com>
2026-08-02 18:07:20 +0000

    Update sheet URL and add PM2 process management setup

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.ROCKWELL_BACKEND_URL = '';
-window.ROCKWELL_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1BbEbzqca1-0A51c5ZJFm6An9XCB8z0fdt4w5T17cH2M/edit?usp=sharing';
+window.ROCKWELL_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1RsgmK5VoY2uQI-636AXH2LvwBHGDDsyp76T1Cu1D37U/edit';
